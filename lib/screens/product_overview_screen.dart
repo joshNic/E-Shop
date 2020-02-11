7eb3a34b416d0f657c2a41e381fdbc21ae15bbf2
joshNic <256jomu@gmail.com>
2020-02-11 10:46:58 +0300

    add multiple providers

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -7,7 +7,7 @@ class ProductsOverviewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('E-Shop'),
+        title: Text('MyShop'),
       ),
       body: ProductsGrid(),
     );
