11478f686554e18b1552fa1b59d447203c41f976
joshNic <256jomu@gmail.com>
2020-02-12 12:58:57 +0300

    adds confirm dialog on cart item deletion

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -54,6 +54,7 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
               Scaffold.of(context).showSnackBar(
                 SnackBar(
                   content: Text('Added item to cart!'),
@@ -62,7 +63,7 @@ class ProductItem extends StatelessWidget {
                     cart.removeSingleItem(product.id);
                   },),
                 )
-              )
+              );
             },
             color: Theme.of(context).accentColor,
           ),
