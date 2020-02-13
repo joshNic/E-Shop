8c837911d1814963dfc1f07438c7f153152f5407
joshNic <256jomu@gmail.com>
2020-02-13 21:25:10 +0300

    add firebase to persist products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -65,6 +67,14 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url = 'https://datacollection-52046.firebaseio.com/products.json';
+    http.post(url,body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite':product.isFavorite,
+    }),);
     final newProduct = Product(
       title: product.title,
       description: product.description,
