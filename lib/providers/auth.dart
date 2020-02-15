35b2b7a22a92d8205d517e61b574863783ed1122
joshNic <256jomu@gmail.com>
2020-02-15 07:17:37 +0300

    authenticate using token

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -58,10 +58,10 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> signup(String email, String password) async {
-    return _authenticate(email, password, 'signupNewUser');
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    return _authenticate(email, password, 'verifyPassword');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
