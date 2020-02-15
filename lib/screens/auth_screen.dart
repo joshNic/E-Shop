7893412ae5cd3fcf7c44c55d331845ec41aa9b76
joshNic <256jomu@gmail.com>
2020-02-15 04:14:31 +0300

    add authentication screen to routes

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -57,7 +57,7 @@ class AuthScreen extends StatelessWidget {
                         ],
                       ),
                       child: Text(
-                        'MyShop',
+                        'E-Shop',
                         style: TextStyle(
                           color: Theme.of(context).accentTextTheme.title.color,
                           fontSize: 50,
